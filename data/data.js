1b98dcaedefa6ef0148b71ec7bcb8a7873d73aaa
keytrain <keytrain@users.noreply.github.com>
2017-05-31 23:35:42 -0700

    init

---
+++ data/data.js
@@ -0,0 +1,480 @@
+window.data = {
+  content: {
+    contact: {
+      phone: '877.448.7711',
+      fax: '714.486.3051',
+      email: 'customerfeedback@myhitmobile.com'
+    },
+    landing: {
+      one: {
+        title: 'CUSTOMERS',
+        text: 'Putting our customers first is our biggest priority. We want to learn about you to make certain you\'re taken care of right the first time.'
+      },
+      two: {
+        title: 'EMPLOYEES',
+        text: 'The backbone and strength of our Company is our employees. Our goal is focused around making a fun and dynamic environment for our team.'
+      }, 
+      three: {
+        title: 'SERVICE',
+        text: 'Changing wireless for good and making sure we take care of our customers. Our focus is to make our customers our long time clients.'
+      },
+      job: {
+        title: '#jointhemovement',
+        text: 'Here at HIT Mobile we support our community members by offering jobs with competitive pay, benefits, flexible schedules, growth and development, and a fun environment.'
+      }
+    },
+    testimonial: {
+      customer: [
+        {
+          text: 'Great service and I like that there are bilingual employees.'
+        },
+        {
+          text: 'I love the way they help each other and helped me get my iPhone 7 Plus. I had a fun experience and I will keep on going there to pay my bills and to get new phones!'
+        },
+        {
+          text: 'Everyone was very professional and friendly.'
+        },
+        {
+          text: 'The associate was eager to help and was very engaging.'
+        },
+        {
+          text: 'Very professional yet friendly... absolutely great customer service.'
+        },
+        {
+          text: 'Associate was very knowledgeable, friendly, and patient. Had very good customer service skills...'
+        },
+        {
+          text: 'This store is great! Very very happy.'
+        },
+        {
+          text: 'All associates were very helpful and welcoming.'
+        }
+      ]
+    },
+    about: {
+      one: {
+        title: 'WHO ARE WE',
+        text: 'HIT Mobile is one of the largest T-Mobile partners in the Southwest with stores conveniently located from Los Angeles, CA to San Diego, CA. We are committed to working hard, having fun, and taking care of our customers!'
+      },
+      two: {
+        title: 'OUR MISSION',
+        text: 'To focus our energy and effort in developing a world class sales team to ensure our customers become our clients.'
+      }, 
+      three: {
+        title: 'OUR VALUES',
+        text: ['Making our customers first', 'Developing our people into the best they can be', 'Having the energy and passion for winning', 'Ensuring everything is done with integrity']
+      }
+    },
+    team: {
+      founders: [
+        {
+          icon: './img/meet/b.jpg',
+          icon2: './img/meet/b2.jpg',
+          name: 'Mauro Martinez',
+          role: 'CEO'
+        },
+        {
+          icon: './img/meet/c.jpg',
+          icon2: './img/meet/c2.jpg',
+          name: 'Sunil Hathiramani',
+          role: 'COO'
+        },
+        {
+          icon: './img/meet/a.jpg',
+          icon2: './img/meet/a2.jpg',
+          name: 'David Kaufman',
+          role: 'CFO'
+        }
+      ],
+      key: [
+        {
+          icon: './img/meet/cv.jpg',
+          name: 'Carlos Vasquez',
+          role: 'Director of Sales - LA'
+        },
+        {
+          icon: './img/meet/tm.jpg',
+          name: 'Tamela Milini',
+          role: 'Director of Sales - SD'
+        },
+        {
+          icon: './img/meet/ab.jpg',
+          name: 'Alma Burgos',
+          role: 'Director of Operations'
+        },
+        {
+          icon: './img/meet/sh.jpg',
+          name: 'Saadiya Horani',
+          role: 'Commission Specialist'
+        },
+        {
+          icon: './img/meet/dq.jpg',
+          name: 'Desiree Quan',
+          role: 'Human Resources Manager'
+        }
+      ]
+    },
+    benefits: [
+      {
+        icon: 'lightbulb-o',
+        text: 'Growth and Development'
+      },
+      {
+        icon: 'tachometer',
+        text: 'Fun and dynamic environment'
+      },
+      {
+        icon: 'money',
+        text: 'Uncapped Commissions'
+      },
+      {
+        icon: 'line-chart',
+        text: 'Career Advancement'
+      },
+      {
+        icon: 'heartbeat',
+        text: 'Health Benefits'
+      },
+      {
+        icon: 'sun-o',
+        text: 'Rest and Relaxation'
+      },
+
+    ],
+    jobs: [
+      {
+        text: 'RETAIL STORE MANAGER',
+        link: './jobs/StoreManager.pdf'
+      },
+      {
+        text: 'ASSISTANT STORE MANAGER',
+        link: './jobs/AssistantStoreManager.pdf'
+      },
+      {
+        text: 'RETAIL STORE ASSOCIATE',
+        link: './jobs/RetailSalesAssociate.pdf'
+      },
+    ]
+  },
+  stores: {
+    la: [
+    {
+      location: 'Anaheim',
+      phone: '657-233-5566',
+      fax: '657-233-5741',
+      address: '1686 W Katella Ave\nAnaheim, CA 92802',
+      cross: 'Katella Ave and Euclid St.',
+      lat: '33.801721',
+      lng: '-117.940651'
+    },
+    {
+      location: 'Baldwin Park',
+      phone: '626-388-9182',
+      fax: '626-851-8165',
+      address: '3060 Baldwin Park Blvd #D101\nBaldwin Park, CA 91706',
+      cross: 'Baldwin Park Blvd and Francisquito',
+      lat: '34.068911',
+      lng: '-117.977484'
+    },
+    {
+      location: 'Bixby Knolls',
+      phone: 'Coming soon!',
+      fax: 'Coming soon!',
+      address: '4512 Atlantic Avenue\nLong Beach, CA 90807',
+      cross: 'Atlantic Ave and E 45th St.',
+      lat: '33.840197',
+      lng: '-118.184362'
+    },
+    {
+      location: 'Covina',
+      phone: '626-966-3263',
+      fax: '626-966-3516',
+      address: '220 N Azusa Ave.\nCovina, CA 91722',
+      cross: 'Azusa Ave and Badillo',
+      lat: '34.087578',
+      lng: '-117.907472'      
+    },
+    {
+      location: 'Crenshaw',
+      phone: '323-545-9644',
+      fax: '323-545-9649',
+      address: '3210 W Saluson Ave.\nLos Angeles, CA 90043',
+      cross: 'Slauson Ave & Crenshaw',
+      lat: '33.988842',
+      lng: '-118.328798'      
+    },
+    {
+      location: 'Cudahy',
+      phone: '323-771-0292',
+      fax: '323-771-3317',
+      address: '7200 Atlantic Ave. Unit B\nCudahy, CA 90201',
+      cross: 'Atlantic Ave. and Florence',
+      lat: '33.969824',
+      lng: '-118.187686'      
+    },
+    {
+      location: 'Downey',
+      phone: 'Coming soon!',
+      fax: 'Coming soon!',
+      address: '8994 Apollo Way\nDowney, CA 90242',
+      cross: 'Apollo Way and Bellflower Blvd',
+      lat: '33.922721',
+      lng: '-118.125753'      
+    },
+    {
+      location: 'Fullerton',
+      phone: '714-992-2210',
+      fax: '714-992-2215',
+      address: '550 N. State College Blvd.\nFullerton, CA 92831',
+      cross: 'State College and Chapman',
+      lat: '33.875476',
+      lng: '-117.888777'      
+    },
+    {
+      location: 'Hawthorne',
+      phone: '310-676-0559',
+      fax: '310-676-0905',
+      address: '12745 Hawthorne Blvd.\nHawthorne, CA 90250',
+      cross: 'Hawthorne and El Segundo Blvd.',
+      lat: '33.916605',
+      lng: '-118.353068'      
+    },
+    {
+      location: 'Inglewood',
+      phone: '310-419-5925',
+      fax: '310-419-4265',
+      address: '425 E. Manchester Blvd #D\nInglewood, CA 90301',
+      cross: 'Manchester and Locust',
+      lat: '33.962264',
+      lng: '-118.349622'      
+    },
+    {
+      location: 'La Puente',
+      phone: '626-917-0420',
+      fax: '626-918-2106',
+      address: '1273 N. Hacienda Blvd #3\nLa Puente, CA 91744',
+      cross: 'Hacienda Blvd and Fairgrove',
+      lat: '34.043494',
+      lng: '-117.950205'      
+    },
+    {
+      location: 'Long Beach 1',
+      phone: '562-424-1762',
+      fax: '562-988-8714',
+      address: '141 E. Willow Street\nLong Beach, CA 90806',
+      cross: 'Long Beach and Willow St.',
+      lat: '33.805574',
+      lng: '-118.190648'      
+    },
+    {
+      location: 'Long Beach 2',
+      phone: '562-354-9200',
+      fax: '562-354-9222',
+      address: '6521 E Spring Street\nLong Beach, CA 90806',
+      cross: 'Spring St. and Palo Verde Ave.',
+      lat: '33.810762',
+      lng: '-118.106303'      
+    },
+    {
+      location: 'Maywood',
+      phone: '',
+      fax: '',
+      address: '8994 Slauson Avenue\nMaywood, CA 90270',
+      cross: 'Slauson and Atlantic',
+      lat: '33.9874467',
+      lng: '-118.1885924'      
+    },
+    {
+      location: 'Monterey Park',
+      phone: '323-780-0177',
+      fax: '323-780-9384',
+      address: '2123 S. Atlantic Blvd\nMonterey Park, CA 91754',
+      cross: 'Atlantic Ave and Floral',
+      lat: '34.038920',
+      lng: '-118.145409'      
+    },
+    {
+      location: 'Northridge',
+      phone: '818-988-2400',
+      fax: '818-998-2401',
+      address: '18497 Nordhoff St.\nNorthridge, CA 91325',
+      cross: 'Reseda and Nordhoff Blvd',
+      lat: '34.236051',
+      lng: '-118.535477'      
+    },
+    {
+      location: 'Panorama',
+      phone: '818-781-1148',
+      fax: '818-781-1325',
+      address: '8244 Van Nuys Blvd.\nPanorama City, CA 91402',
+      cross: 'Van Nuys and Titus',
+      lat: '34.220922',
+      lng: '-118.448491'      
+    },
+    {
+      location: 'Santa Ana',
+      phone: '714-541-4520',
+      fax: '714-541-4526',
+      address: '1313 W 17th Street\nSanta Ana, CA 92706',
+      cross: '17th and Bristol St.',
+      lat: '33.760343',
+      lng: '-117.886243'      
+    },
+    {
+      location: 'Torrance',
+      phone: '310-371-0200',
+      fax: '310-371-0201',
+      address: '18425 Hawthorne Blvd.\nTorrance, CA 90504',
+      cross: 'Hawthrone and 182nd St.',
+      lat: '33.863429',
+      lng: '-118.352690'      
+    },
+    {
+      location: 'Westminster',
+      phone: '714-899-8350',
+      fax: '714-899-8739',
+      address: '6777 Westminster Blvd\nWestminster, CA 92683',
+      cross: 'Westminster and Goldenwest',
+      lat: '33.761474',
+      lng: '-118.009705'      
+    },
+    {
+      location: 'Whittier',
+      phone: '562-205-1400',
+      fax: '562-205-1407',
+      address: '11133 Washington Blvd\nWhittier, CA 90606',
+      cross: 'Washington Blvd and Norwalk',
+      lat: '33.9733699',
+      lng: '-118.07267389999998'   
+    }
+    ],
+    sd: [
+    {
+      location: 'Chula Broadway',
+      phone: '619-691-9100',
+      fax: '619-691-9101',
+      address: '542 Broadway #A&B\nChula Vista, CA 91910',
+      cross: 'Broadway and H St.',
+      lat: '32.629223',
+      lng: '-117.090775'      
+    },
+    {
+      location: 'Chula Mall',
+      phone: '619-422-4079',
+      fax: '619-422-2412',
+      address: '542 Broadway #1004\nChula Vista, CA 91910',
+      cross: 'Broadway and H St.',
+      lat: '32.629195',
+      lng: '-117.089686'      
+    },
+    {
+      location: 'Clairemont',
+      phone: '858-270-3500',
+      fax: '858-270-3509',
+      address: '3592 Clairemont Mesa Blvd. #C\nSan Diego, CA 92117',
+      cross: 'Clairemont Mesa Blvd. and Rolfe Rd.',
+      lat: '32.827768',
+      lng: '-117.208853'      
+    },
+    {
+      location: 'El Cajon 1',
+      phone: '619-590-1900',
+      fax: '619-590-1989',
+      address: '354 N. 2nd Street\nEl Cajon, CA 92021',
+      cross: 'N 2nd and Kumeyaay Hwy',
+      lat: '32.799011',
+      lng: '-116.936626'      
+    },
+    {
+      location: 'El Cajon 2',
+      phone: '',
+      fax: '',
+      address: '139 N Magnolia Avenue\nEl Cajon, CA 92020',
+      cross: 'Magnolia Ave. and Rea Ave.',
+      lat: '32.7957328',
+      lng: '-116.96228429999996'      
+    },
+    {
+      location: 'Escondido',
+      phone: '760-747-1033',
+      fax: '760-747-1080',
+      address: '1201 E. Valley Parkway #C\nEscondido, CA 92027',
+      cross: 'Valley Pkwy and Ash',
+      lat: '33.130274',
+      lng: '-117.066573'      
+    },
+    {
+      location: 'Highland',
+      phone: '619-656-9300',
+      fax: '619-656-9333',
+      address: '3007 Highland Ave #109\nNational City, CA 91950',
+      cross: 'Highland Ave and 30th',
+      lat: '32.6588188',
+      lng: '-117.0888635'      
+    },
+    {
+      location: 'La Jolla',
+      phone: '',
+      fax: '',
+      address: '8801 Villa La Jolla Drive\nLa Jolla, CA 92037',
+      cross: 'Villa La Jolla Dr. and Nobel Dr.',
+      lat: '32.8681139',
+      lng: '-117.2329019'      
+    },
+    {
+      location: 'Murphy Canyon',
+      phone: '858-694-0474',
+      fax: '858-694-0479',
+      address: '3755 Murphy Canyon Road #A\nSan Diego, CA 92123',
+      cross: 'Murphy Canyon Rd. and Aero Dr.',
+      lat: '32.810820',
+      lng: '-117.116016'      
+    },
+    {
+      location: 'Oceanside',
+      phone: '',
+      fax: '',
+      address: '463 College Blvd Unit N\nOceanside, CA 92057',
+      cross: 'College Blvd and Hwy 76',
+      lat: '33.244438',
+      lng: '-117.29245300000002'      
+    },
+    // {
+    //   location: 'National City',
+    //   phone: '619-477-7278',
+    //   fax: '619-477-7328',
+    //   address: '3007 Highland Ave. #109\nNational City, CA 91950',
+    //   cross: 'Highland Ave. and 30th',
+    //   lat: '32.658819',
+    //   lng: '-117.088864'      
+    // },
+    {
+      location: 'San Marcos 1',
+      phone: '760-598-7209',
+      fax: '760-598-9740',
+      address: '700 S. Rancho Santa Fe Rd. #A\nSan Marcos, CA 92078',
+      cross: 'Rancho Santa Fe and San Marcos Blvd.',
+      lat: '33.132939',
+      lng: '-117.207828'      
+    },
+    {
+      location: 'San Marcos 2',
+      phone: 'Coming soon!',
+      fax: 'Coming soon!',
+      address: '595 Grand Ave. #100\nSan Marcos, CA 92078',
+      cross: 'Grand Ave. and Creekside Place',
+      lat: '33.136102',
+      lng: '-117.178264'      
+    },
+    {
+      location: 'Telegraph',
+      phone: '619-656-9300',
+      fax: '619-656-9333',
+      address: '523 Telegraph Canyon Rd.\nChula Vista, CA 91911',
+      cross: 'Telegraph Canyon Rd. and I-805',
+      lat: '32.629148',
+      lng: '-117.039618'      
+    }
+    ]
+  }
+}
